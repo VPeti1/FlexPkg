49fb783c6f0931c8951e40f4d86aa80a31a9152a
Vasko Peter <vpeti011@outlook.com>
2024-04-02 10:01:33 +0200

    V8

--- osr.py
+++ osr.py
@@ -1,6 +1,7 @@
 import time
 import platform
 import sys
+import os
 
 def detect_os():
     os_name = platform.system()
@@ -10,54 +11,162 @@ def detect_os():
         return "windows"
 
 
-def main(os):
-    os = int(os)  # Convert os to an integer
-    if os == 0:
+def fail():
+    print("Unsupported FLEXPKG file for this os!")
+    time.sleep(0.5)
+    sys.exit(1)
+
+def main(osreturn):
+    osreturn = int(osreturn)  # Convert osreturn to an integer
+    if osreturn == 0:
         pass
-    elif os == 1:
+    elif osreturn == 1:
         system_type = detect_os()
         if system_type == "windows":
-            print("Unsupported FLEXPKG file for this os!")
-            time.sleep(1.5)
-            sys.exit(1)
+            fail()
             
-        elif system_type == "linux":
+        else:
             sys.exit(0)
 
-        else:
-            pass
-    elif os == 2:
+    elif osreturn == 7:
         system_type = detect_os()
         if system_type == "windows":
             sys.exit(0)
-        elif system_type == "linux":
-            print("Unsupported FLEXPKG file for this os!")
-            time.sleep(1.5)
-            sys.exit(1)
         else:
-            pass
+            fail()
+
+    elif osreturn == 2:
+        system_type = detect_os()
+        if system_type == "windows":
+            fail()
+            
+        else:
+            if os.path.isfile('/usr/flex/arch.cw'):
+                sys.exit(0)
+            elif os.path.isfile('/usr/flex/deb.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/fed.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/suse.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/void.cw'):
+                fail()
+            else:
+                print("Failed to run OSR funtion!")
+                time.sleep(0.5)
+                sys.exit(1)
+    elif osreturn == 3:
+        system_type = detect_os()
+        if system_type == "windows":
+            fail()
+            
+        else:
+            if os.path.isfile('/usr/flex/arch.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/deb.cw'):
+                sys.exit(0)
+            elif os.path.isfile('/usr/flex/fed.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/suse.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/void.cw'):
+                fail()
+            else:
+                print("Failed to run OSR funtion!")
+                time.sleep(0.5)
+                sys.exit(1)
+    elif osreturn == 4:
+        system_type = detect_os()
+        if system_type == "windows":
+            fail()
+            
+        else:
+            if os.path.isfile('/usr/flex/arch.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/deb.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/fed.cw'):
+                sys.exit(0)
+            elif os.path.isfile('/usr/flex/suse.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/void.cw'):
+                fail()
+            else:
+                print("Failed to run OSR funtion!")
+                time.sleep(0.5)
+                sys.exit(1)
+    elif osreturn == 5:
+        system_type = detect_os()
+        if system_type == "windows":
+            fail()
+            
+        else:
+            if os.path.isfile('/usr/flex/arch.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/deb.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/fed.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/suse.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/void.cw'):
+                sys.exit(0)
+            else:
+                print("Failed to run OSR funtion!")
+                time.sleep(0.5)
+                sys.exit(1)
+    elif osreturn == 6:
+        system_type = detect_os()
+        if system_type == "windows":
+            fail()
+            
+        else:
+            if os.path.isfile('/usr/flex/arch.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/deb.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/fed.cw'):
+                fail()
+            elif os.path.isfile('/usr/flex/suse.cw'):
+                sys.exit(0)
+            elif os.path.isfile('/usr/flex/void.cw'):
+                fail()
+            else:
+                print("Failed to run OSR funtion!")
+                time.sleep(0.5)
+                sys.exit(1)
     else:
-        pass
+        print("Failed to run OSR funtion!")
+        time.sleep(0.5)
+        sys.exit(1)
 
 def check_osr_line(filename):
     try:
         with open(filename, 'r') as file:
             for line in file:
                 if "osr = " in line:
-                    # Extract the text after "osr = "gn
+                    # Extract the text after "osr = "
                     text_after_osr = line.split("osr = ")[1].strip()
                     if text_after_osr == "lin":
                         os = "1"
-                    else:
+                    elif text_after_osr == "linarch":
                         os = "2"
+                    elif text_after_osr == "lindeb":
+                        os = "3"
+                    elif text_after_osr == "linfed":
+                        os = "4"
+                    elif text_after_osr == "linvoid":
+                        os = "5"
+                    elif text_after_osr == "linsuse":
+                        os = "6"
+                    else:
+                        os = "7"
                     break  # Stop searching after finding the first matching line
             else:
                 # If no matching line is found
                 os = "0"
-            a = os
-            main(a)
+            main(os)
     except FileNotFoundError:
         pass
-
 
 check_osr_line("flex.pkg")
